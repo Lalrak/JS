ff1572dc249cede00002eafb9949600c586c9f17
Lalrak <150138865+Lalrak@users.noreply.github.com>
2025-06-13 19:49:17 -0300

    Inicial - Alura JS

---
+++ A partir do zero iniciante em programação/logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao jogo do mundo secreto');
+let numeroMaximo = alert('Digite o valor máximo para o jogo')
+let numeroSecreto = parseInt(Math.random() * parseInt(numeroMaximo)) + 1;
+let chute;
+let tentativa = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+    if (numeroSecreto == chute) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+    tentativa++;
+}
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Isso ai! Você descobriu o número secreto ${chute} com ${tentativa} ${palavraTentativa}`);
