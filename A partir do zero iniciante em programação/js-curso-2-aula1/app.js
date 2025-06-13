859e829bedb7c0622c476acbf2dbe458662178a4
Lalrak <150138865+Lalrak@users.noreply.github.com>
2025-06-13 20:26:32 -0300

    logica-programacao-funcoes-listas aula 2e 3

--- A partir do zero iniciante em programação/js-curso-2-aula1/app.js
+++ A partir do zero iniciante em programação/js-curso-2-aula1/app.js
@@ -1,9 +1,48 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'Roleta Russa';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero entre 1 e 10'
-
-function verificarChute(){
-    
+let numero = gerarNumeroAleatorio();
+let tentativas = 1;
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
+
+function mensagemInicial(){
+    exibirTextoNaTela('h1', 'Roleta Russa');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numero) {
+        exibirTextoNaTela('h1', 'acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numero) {
+            exibirTextoNaTela('p', 'O número é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numero = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true)
+}
+mensagemInicial();
