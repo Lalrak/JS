92d6e39d86c1ac957b86749f4c2d9a2f561032ad
JoaoP <joaopaulo.silva2021@gmail.com>
2025-06-18 18:49:52 -0300

    feat: adiciona arquivo tipo-null-undefined.js com exemplos de uso de null e undefined

---
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/1 - JS- utilizando tipos/tipo-null-undefined.js
@@ -0,0 +1,11 @@
+let nomeEstudante;
+const professora = 'Ana';
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+console.log(typeof professora);
+
+let telefoneEstudante = null;
+
+console.log(typeof telefoneEstudante);
+
+// NaN - NaN significa "Not a Number". Indica que o resultado de uma operação não é um número válido, geralmente devido a operações matemáticas indefinidas, como 0/0 ou a raiz quadrada de um número negativo.
