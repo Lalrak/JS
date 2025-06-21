6e66b7953a1be2df69a30347eb67f4d1f9001e58
Lalrak <150138865+Lalrak@users.noreply.github.com>
2025-06-21 17:57:34 -0300

    feat: adiciona manipulação de objetos e conversão de JSON para exibição de dados de estudantes e animais

---
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/3 - JS - conhecendo objetos/converte-json.js
@@ -0,0 +1,8 @@
+const estudante = require("./estudante.json");
+
+const stringEstudante = JSON.stringify(estudante);
+// console.log(stringEstudante);
+// console.log(typeof stringEstudante);
+
+const objetoEstudante = JSON.parse(stringEstudante);
+console.log(objetoEstudante);
---
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/3 - JS - conhecendo objetos/spread.js
@@ -0,0 +1,34 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript',
+  bolsista: true,
+  telefones: ['55119549998', '55119579223', '55117893434'],
+  enderecos: [
+    {
+      rua: 'Rua Joseph Climber',
+      numero: '45',
+      complemento: 'apto 43',
+    },
+    {
+      rua: 'Rua Dona Clotilde',
+      numero: '71',
+      complemento: null,
+    },
+  ],
+};
+
+function exibirTelefones(telefone1, telefone2) {
+  console.log(`ligar para: ${telefone1}`);
+  console.log(`ligar para: ${telefone2}`);
+}
+
+exibirTelefones(estudante.telefones[0], estudante.telefones[1]);
+exibirTelefones(...estudante.telefones);
+
+const dadosEnvio = {
+  destinatario: estudante.nome,
+  ...estudante.enderecos[0],
+};
+console.log(dadosEnvio);
