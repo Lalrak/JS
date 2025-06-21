e54d11c55474ec5fb6f6fed3837419ab1be5bcb4
Lalrak <150138865+Lalrak@users.noreply.github.com>
2025-06-21 15:11:30 -0300

    feat: adiciona configurações do editor e novos scripts para manipulação de objetos

--- 2 - Aprenda a programar em JavaScript com foco no back-end/3 - JS - conhecendo objetos/alterando-objeto.js
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/3 - JS - conhecendo objetos/alterando-objeto.js
@@ -2,8 +2,8 @@ const estudante = {
   nome: 'José Silva',
   idade: 32,
   cpf: '12312312312',
-  turma: 'JavaScript'
-}
+  turma: 'JavaScript',
+};
 
 estudante.telefone = '551199999999';
 console.log(estudante.telefone);
@@ -13,5 +13,5 @@ estudante.nome = 'José Souza';
 console.log(estudante);
 
 const estudante2 = {};
-estudante2.nome = 'Ana Maria'
+estudante2.nome = 'Ana Maria';
 console.log(estudante2);
---
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/3 - JS - conhecendo objetos/objs-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript',
+  bolsista: true,
+  telefones: ['551199999998', '551199999993'],
+};
+
+estudante.edereco = {
+  rua: 'Rua Joseph',
+  numero: 123,
+  complemento: 'apto 43',
+};
+
+console.log(estudante.edereco.rua);
