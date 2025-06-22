5ad44d8e57cc4b610ebe81b4f2799bea71d79fe1
Lalrak <150138865+Lalrak@users.noreply.github.com>
2025-06-22 08:29:29 -0300

    feat: adiciona função para criar e salvar arquivo com palavras duplicadas

--- 2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/cli.js
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/cli.js
@@ -1,16 +1,48 @@
 import fs from "fs";
 import trataErros from "./erro/funcoesErro.js";
 import { contaPalavras } from "./index.js";
+import { montaSaidaArquivo } from "./helpers.js";
 
 const caminhoArquivo = process.argv;
 const link =
   "2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/arquivos/texto-web.txt"; //caminhoArquivo[2]
+const endereco =
+  "2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/resultados";
 
 fs.readFile(link, "utf-8", (erro, texto) => {
   try {
     if (erro) throw erro;
-    contaPalavras(texto);
+    const resultado = contaPalavras(texto);
+    criaESalvaArquivo(resultado, endereco);
   } catch (erro) {
     trataErros(erro);
   }
 });
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = montaSaidaArquivo(listaPalavras);
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log("Arquivo criado com sucesso");
+  } catch (erro) {
+    throw erro;
+  }
+}
+
+// function criaESalvaArquivo(listaPalavras, endereco) {
+//   const arquivoNovo = `${endereco}/resultado.txt`;
+//   const textoPalavras = JSON.stringify(listaPalavras);
+
+//   fs.promises
+//     .writeFile(arquivoNovo, textoPalavras)
+//     .then(() => {
+//       console.log("Arquivo criado com sucesso");
+//     })
+//     .catch((erro) => {
+//       throw erro;
+//     })
+//     .finally(() => {
+//       console.log("Processo finalizado");
+//     });
+// }
---
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/testes.js
@@ -0,0 +1,15 @@
+const caminhos = [
+  "./arquivos/texto-kanban.txt",
+  "./arquivos/texto-web.txt",
+  "./arquivos/texto-aprendizado.txt",
+];
+
+async function lerMultiplosArquivos(arrayDeCaminhos) {
+  const arrayDePromessas = arrayDeCaminhos.map(
+    async (caminho) => await fs.promises.readFile(caminho, "utf-8")
+  );
+  const conteudosDosArquivos = await Promise.all(arrayDePromessas);
+  return conteudosDosArquivos;
+}
+
+lerMultiplosArquivos(caminhos);
