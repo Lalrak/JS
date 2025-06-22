8c8b350e9b5fce3c4edb687e9403908216b66cb9
Lalrak <150138865+Lalrak@users.noreply.github.com>
2025-06-21 21:27:47 -0300

    feat: adiciona função para contar palavras em um texto e manipular parágrafos

--- 2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/index.js
+++ 2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/index.js
@@ -1,4 +1,45 @@
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
+const fs = require("fs");
 
-console.log(caminhoArquivo);
+const caminhoArquivo = process.argv;
+const link =
+  "2 - Aprenda a programar em JavaScript com foco no back-end/4 - NodeJS - Lib/arquivos/texto-web.txt"; //caminhoArquivo[2]
+
+fs.readFile(link, "utf-8", (erro, texto) => {
+  try {
+    if (erro) throw erro;
+    contaPalavras(texto);
+  } catch (erro) {
+    console.log();
+  }
+});
+
+function contaPalavras(texto) {
+  const paragrafos = extraiParagrafo(texto);
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  });
+  console.log(contagem);
+}
+
+function extraiParagrafo(texto) {
+  return texto.toLowerCase().split("\n");
+}
+
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+
+  return resultado;
+}
